52a4359074eebd5c720b1017851aa8958c93aedf
r1chwo0n <woonzy09@gmail.com>
2023-01-17 16:51:57 +0700

    lab15_2 completed

--- lab15_2.cpp
+++ lab15_2.cpp
@@ -35,18 +35,18 @@ void inputMatrix(double A[][N]){
 }
 
 void findLocalMax(const double A[][N], bool B[][N]){
-    for(int i = 0; i<N; i++) B[0][i] = 0;
-    for(int i = 0; i<N; i++) B[N-1][i] = 0;
-    for(int i = 0; i<N; i++) B[i][0] = 0;
-    for(int i = 0; i<N; i++) B[i][N-1] = 0;
+    for(int i = 0; i<N; i++) B[0][i] = false;
+    for(int i = 0; i<N; i++) B[N-1][i] = false;
+    for(int i = 0; i<N; i++) B[i][0] = false;
+    for(int i = 0; i<N; i++) B[i][N-1] = false;
     
     for(int i = 1; i < N-1; i++){
         for(int j = 1; j < N-1; j++) {
-        if(A[i][j] < A[i-1][j]) B[i][j] = false;
-        else if(A[i][j] < A[i+1][j]) B[i][j] = false;
-        else if(A[i][j] < A[i][j-1]) B[i][j] = false;
-        else if(A[i][j] < A[i][j+1]) B[i][j] = false;
-        else B[i][j] = true;
+        	if(A[i][j] < A[i-1][j]) B[i][j] = false;
+        	else if(A[i][j] < A[i+1][j]) B[i][j] = false;
+        	else if(A[i][j] < A[i][j-1]) B[i][j] = false;
+        	else if(A[i][j] < A[i][j+1]) B[i][j] = false;
+        	else B[i][j] = true;
         }
     }
 }
